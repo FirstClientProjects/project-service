8b1b65840b2555bb250c4c3f32d1e510d0dca7df
FirstClientProjects <firstclient646@gmail.com>
2024-07-04 16:55:44 +0530

    Create model

--- src/api/models/Project.js
+++ src/api/models/Project.js
@@ -1,7 +1,57 @@
 const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true
+    },
+    organization: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization'
+    },
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client'
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    website: {
+        type: string,
+        required: true
+    },
+    repositoryLink: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ['Not Started', 'In Progress', 'Completed', 'On Hold', 'Finished'],
+        default: 'Not Started'
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    deployedDate: {
+        type: Date,
+    },
+    endDate: {
+      type: Date
+    },
+    budget: {
+        type: Number,
+        required: true
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Normal', 'High', 'Critical'],
+        default: 'Medium'
+    },
+    teamMembers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Developer'
+    }],
 }, { timestamps: true });
 
 module.exports = mongoose.model('Project', ProjectSchema);
